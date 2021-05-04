1ac8b3f111fcce2804a62c518f96d3a41e300142
mohamadrezarahzani <83678935+mohamadrezarahzani@users.noreply.github.com>
2021-05-05 04:22:37 +0430

    mohamad.py
    به زووی

---
+++ saat.py
@@ -0,0 +1,8 @@
+
+print("{}        ____ ").format(r)
+print("   _[]_/____\__n_ ")
+print("  |_____.--.__()_|")
+print("  |I   //# \\\    |")
+print("{}  |P   \\\__//    | ").format(w)
+print("  |CS   '--'     | ")
+print("به زودی")
